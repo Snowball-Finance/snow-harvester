ca6f5b4792d2c8e5ff8b5e725814e16e93b331fa
Jonasslv <jowsba@hotmail.com>
2021-09-10 13:43:15 -0300

    added config file

--- config/Config.js
+++ config/Config.js
@@ -3,7 +3,7 @@ const HOUR = 60 * MINUTE;
 
 const Config = {
     WALLET: {
-        ADDRESS: '0x096a46142C199C940FfEBf34F0fe2F2d674fDB1F', // Sample address for easy runs in test mode
+        ADDRESS: '0x096a46142C199C940FfEBf34F0fe2F2d674fDB1F',
         KEY: process.env.SNOWBALL_KEY,
     },
     EXECUTION: {
